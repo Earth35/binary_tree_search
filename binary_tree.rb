c3916c062236e439f49c50cd2936de15152b251d
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-14 21:09:23 +0200

    Breadth First Search method complete

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,21 +1,33 @@
 class BinarySearchTree
   attr_reader :array
   attr_accessor :root
+  
   def initialize
     @array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
     @root = build_tree(@array)
   end
   
-  class Node
-    attr_reader :value
-    attr_accessor :left, :right
-    def initialize (value)
-      @value = value
-      @left = nil
-      @right = nil
+  def breadth_first_search(target)
+    queue = []
+    # check if root is the traget node
+    if @root.value == target
+      return @root
+    else
+      queue_children_nodes(@root, queue)
     end
+    until queue.empty?  # check queued nodes (First In - First Out), return node if found the target value
+      current_node = queue.shift
+      if current_node.value == target
+        return current_node
+      else
+        queue_children_nodes(current_node, queue)
+      end
+    end
+    return nil  # return nil if target not present in the tree
   end
-
+  
+  private
+  
   def build_tree (array, root = nil)
     until array.empty?
       new_value = array.shift
@@ -44,9 +56,23 @@ class BinarySearchTree
       end
     end
   end
+  
+  def queue_children_nodes (parent, queue)
+    # push only existing nodes
+    queue.push(parent.left) if parent.left
+    queue.push(parent.right) if parent.right
+  end
+  
+  class Node
+    attr_reader :value
+    attr_accessor :left, :right
+    def initialize (value)
+      @value = value
+      @left = nil
+      @right = nil
+    end
+  end
 end
 
-
 binary_search_tree = BinarySearchTree.new
-
-
+puts binary_search_tree.breadth_first_search(67)
