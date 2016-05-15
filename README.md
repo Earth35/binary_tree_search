61f54a933fa3da24e9d1bf63ce74899e381b0ee8
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-15 11:12:00 +0200

    DFS complete (iterative and recursive)

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 #Binary tree  
-A simple algorithm for building binary trees and locating data inside them.  
+A simple algorithm for building binary search trees and locating data inside them with:  
+ - Breadth First Algorithm  
+ - Depth First Algorithm (iterative and recursive implementation)  
+  
 Data structures & algorithm exercise:  
 http://www.theodinproject.com/ruby-programming/data-structures-and-algorithms  
   
--- binary_tree.rb
+++ binary_tree.rb
@@ -7,23 +7,55 @@ class BinarySearchTree
     @root = build_tree(@array)
   end
   
-  def breadth_first_search(target)
+  def breadth_first_search (target) # BFS
     queue = []
     # check if root is the traget node
     if @root.value == target
       return @root
     else
-      queue_children_nodes(@root, queue)
+      push_children_nodes(@root, queue)
     end
     until queue.empty?  # check queued nodes (First In - First Out), return node if found the target value
       current_node = queue.shift
       if current_node.value == target
         return current_node
       else
-        queue_children_nodes(current_node, queue)
+        push_children_nodes(current_node, queue)
       end
     end
     return nil  # return nil if target not present in the tree
+  end
+  
+  def depth_first_search (target) # iterative DFS
+    stack = []
+    # check the root node
+    if @root.value == target
+      return @root
+    else
+      push_children_nodes(@root, stack)
+    end
+    until stack.empty?  # start checking nodes from the stack
+      current_node = stack.pop
+      if current_node.value == target
+        return current_node
+      else
+        push_children_nodes(current_node, stack)
+      end
+    end
+    return nil # return nil if target not found
+  end
+  
+  def dfs_rec (target, current_node = @root) # recursive DFS
+    return current_node if current_node.value == target # returns own value if equal to target
+    left_child = current_node.left ? dfs_rec(target, current_node.left) : nil
+    # return the value of child node if equal to target
+    if left_child
+      return left_child if left_child.value == target
+    end
+    right_child = current_node.right ? dfs_rec(target, current_node.right) : nil
+    if right_child
+      return right_child if right_child.value == target
+    end
   end
   
   private
@@ -32,7 +64,7 @@ class BinarySearchTree
     until array.empty?
       new_value = array.shift
       if !root
-        root = Node.new(new_value)  # element under index 0 will always serve as root since we assume that array cannot be sorted
+        root = Node.new(new_value)  # element under index 0 will always serve as root since we assume the array cannot be sorted
       else
         place_node(new_value, root)
       end
@@ -48,7 +80,7 @@ class BinarySearchTree
         place_node(value, root.left)
       end
     elsif value > root.value
-      # perform an analogous operation if analyzed element is greater than root's value; duplicates aren't placed
+      # perform an analogous operation if analyzed element is greater than root value; duplicates aren't placed
       if root.right.nil?
         root.right = Node.new(value)
       else
@@ -57,10 +89,10 @@ class BinarySearchTree
     end
   end
   
-  def queue_children_nodes (parent, queue)
-    # push only existing nodes
-    queue.push(parent.left) if parent.left
-    queue.push(parent.right) if parent.right
+  def push_children_nodes (parent, array)
+    # push only existing nodes; array is a queue (BFS) or a stack (DFS)
+    array.push(parent.right) if parent.right
+    array.push(parent.left) if parent.left
   end
   
   class Node
@@ -75,4 +107,9 @@ class BinarySearchTree
 end
 
 binary_search_tree = BinarySearchTree.new
+puts "BFS:"
 puts binary_search_tree.breadth_first_search(67)
+puts "Iterative DFS:"
+puts binary_search_tree.depth_first_search(67)
+puts "Recursive DFS:"
+puts binary_search_tree.dfs_rec(67)
